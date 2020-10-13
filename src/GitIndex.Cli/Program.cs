48a81880d8e290d542045abd0b4f422f72f77627
Matthew John Cheetham <mjcheetham@outlook.com>
2020-10-13 13:00:57 +0100

    cmd: use System.CommandLine to add cmdline arg parsing

--- src/GitIndex.Cli/Program.cs
+++ src/GitIndex.Cli/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Globalization;
+using System.Linq;
 
 namespace Mjcheetham.Git.IndexViewer.Cli
 {
@@ -7,15 +12,91 @@ namespace Mjcheetham.Git.IndexViewer.Cli
     {
         public static void Main(string[] args)
         {
-            if (args.Length > 0 && StringComparer.OrdinalIgnoreCase.Equals("--help", args[0]))
+            var rootCommand = new RootCommand("Utility for inspecting the Git index file.")
             {
-                Console.WriteLine("usage: git-index [<path>]");
-                return;
+                new Option<string>(new[] {"-i", "--index"})
+                {
+                    Description = "Path to a Git repository or .git/index file",
+                }
+            };
+
+            var infoCommand = new Command("info", "Show summary information about the index");
+            var listCommand = new Command("list", "List and filter index entries")
+            {
+                new Argument("path")
+                {
+                    Description = "Filter based on path prefix",
+                    Arity = ArgumentArity.ZeroOrOne
+                },
+                new Option<bool>("--ignore-case", "Perform case-insensitive filtering on paths"),
+                new Option<bool>("--no-table", "Do not show table headings or columns"),
+            };
+
+            rootCommand.Add(infoCommand);
+            rootCommand.Add(listCommand);
+
+            infoCommand.Handler = CommandHandler.Create<string>(Info);
+            listCommand.Handler = CommandHandler.Create<string, string, bool, bool>(List);
+
+            int exitCode = rootCommand.Invoke(args);
+            Environment.Exit(exitCode);
+        }
+
+        private static int Info(string indexFile)
+        {
+            if (!TryGetIndexFile(indexFile, out string filePath))
+            {
+                Console.Error.WriteLine("Unable to locate index file.");
+                return 1;
             }
 
-            string filePath = args.Length > 0
-                ? Path.GetFullPath(args[0])
-                : Directory.GetCurrentDirectory();
+            Index index = IndexSerializer.Deserialize(filePath);
+            PrintSummary(index, filePath);
+            return 0;
+        }
+
+        private static int List(string indexFile, string path, bool ignoreCase, bool noTable)
+        {
+            if (!TryGetIndexFile(indexFile, out string filePath))
+            {
+                Console.Error.WriteLine("Unable to locate index file.");
+                return 1;
+            }
+
+            Index index = IndexSerializer.Deserialize(filePath);
+
+            int consoleWidth = Math.Max(Console.WindowWidth, 120);
+
+            const string PathColHeader = "Path";
+            const string OtherColHeaders = " | Mode       | OID          | SkipWT | Add   | Stage          ";
+
+            int pathExtraWidth = consoleWidth - OtherColHeaders.Length - PathColHeader.Length;
+
+            if (!noTable)
+            {
+                Console.WriteLine("{0}{1}{2}", PathColHeader, new string(' ', pathExtraWidth), OtherColHeaders);
+                Console.WriteLine(new string('-', consoleWidth));
+            }
+
+            IEnumerable<IndexEntry> entries = index.Entries;
+            if (!string.IsNullOrWhiteSpace(path))
+            {
+                entries = index.Entries.Where(x => x.Path.StartsWith(path, ignoreCase, CultureInfo.InvariantCulture));
+            }
+
+            foreach (IndexEntry entry in entries)
+            {
+                PrintEntry(entry, consoleWidth - OtherColHeaders.Length, noTable);
+            }
+
+            return 0;
+        }
+
+        private static bool TryGetIndexFile(string arg, out string filePath)
+        {
+            filePath = string.IsNullOrWhiteSpace(arg)
+                ? Directory.GetCurrentDirectory()
+                : Path.GetFullPath(arg);
 
             string fileName = Path.GetFileName(filePath);
 
@@ -25,40 +106,22 @@ namespace Mjcheetham.Git.IndexViewer.Cli
                 if (File.Exists(p))
                 {
                     filePath = p;
+                    return true;
                 }
-                else
+
+                p = Path.Combine(filePath, ".git", "index");
+                if (File.Exists(p))
                 {
-                    p = Path.Combine(filePath, ".git", "index");
-                    if (File.Exists(p))
-                    {
-                        filePath = p;
-                    }
-                    else
-                    {
-                        Console.Error.WriteLine("Unable to locate index file.");
-                        Environment.Exit(1);
-                    }
+                    filePath = p;
+                    return true;
                 }
             }
-
-            Index index = IndexSerializer.Deserialize(filePath);
-
-            PrintSummary(index, filePath);
-            Console.WriteLine();
-
-            int consoleWidth = Math.Max(Console.WindowWidth, 120);
-
-            const string PathColHeader = "Path";
-            const string OtherColHeaders = " | Mode       | OID          | SkipWT | Add   | Stage          ";
-
-            int pathExtraWidth = consoleWidth - OtherColHeaders.Length - PathColHeader.Length;
-
-            Console.WriteLine("{0}{1}{2}", PathColHeader, new string(' ', pathExtraWidth), OtherColHeaders);
-            Console.WriteLine(new string('-', consoleWidth));
-            foreach (IndexEntry entry in index.Entries)
+            else if (File.Exists(filePath))
             {
-                PrintEntry(entry, consoleWidth - OtherColHeaders.Length);
+                return true;
             }
+
+            return false;
         }
 
         private static void PrintSummary(Index index, string filePath)
@@ -72,12 +135,13 @@ namespace Mjcheetham.Git.IndexViewer.Cli
             Console.WriteLine("Extensions : {0} bytes", index.Extensions.Length);
         }
 
-        private static void PrintEntry(IndexEntry entry, int maxPath)
+        private static void PrintEntry(IndexEntry entry, int maxPath, bool noTable)
         {
-            string formatString = $"{{0,-{maxPath}}} | {{1,-10}} | {{2,-12}} | {{3,-6}} | {{4,-5}} | {{5}}";
+            char colDividerChar = noTable ? ' ' : '|';
+            string formatString = $"{{1,-{maxPath}}} {{0}} {{2,-10}} {{0}} {{3,-12}} {{0}} {{4,-6}} {{0}} {{5,-5}} {{0}} {{6}}";
             string path = TruncatePath(entry.Path, maxPath);
 
-            Console.WriteLine(formatString,
+            Console.WriteLine(formatString, colDividerChar,
                 path, entry.Status.Mode, entry.ObjectId.ToString(12), entry.SkipWorktree, entry.IntentToAdd, entry.Stage);
         }
 
